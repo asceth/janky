73abb46119aacf0e9933f0254c820a5655a98b61
Iristyle <Iristyle@github>
2013-01-02 17:52:40 -0500

    Added start messages to chat service
    It's incredibly useful to know that Janky is succesfully handling
    build requests based on GitHub notifications.

--- lib/janky/notifier/chat_service.rb
+++ lib/janky/notifier/chat_service.rb
@@ -1,6 +1,18 @@
 module Janky
   module Notifier
     class ChatService
+      def self.started(build)
+        message = "Build #%s (%s) of %s/%s started %s" % [
+          build.number,
+          build.short_sha1,
+          build.repo_name,
+          build.branch_name,
+          build.branch_url
+        ]
+
+        ::Janky::ChatService.speak(message, build.room_id, {:color => "green"})
+      end
+
       def self.completed(build)
         status = build.green? ? "was successful" : "failed"
         color = build.green? ? "green" : "red"
